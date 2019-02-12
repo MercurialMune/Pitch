86c4d7d5db8f5cf9cfd8221efdaf620d84981172
Cyprian Munene <munenecyp@gmail.com>
2019-02-12 10:01:08 +0300

    add a README.md file

---
+++ app/models.py
@@ -0,0 +1,16 @@
+class Pitch:
+    '''
+    News class to define Pitch Objects
+    '''
+
+    def __init__(self, id ):
+        self.id= id
+
+
+class User:
+    '''
+        News class to define User Objects
+        '''
+
+    def __init__(self, id):
+        self.id = id
