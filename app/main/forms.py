f248deebabcc54e61aab385a712aa66d2bb66450
Cyprian Munene <munenecyp@gmail.com>
2019-02-15 02:10:37 +0300

    edit all method tests

--- app/main/forms.py
+++ app/main/forms.py
@@ -19,3 +19,9 @@ class RegisterForm(FlaskForm):
 class UpdateProfile(FlaskForm):
     bio = TextAreaField('Tell us about you.',validators = [Required()])
     submit = SubmitField('Submit')
+
+class PitchForm(FlaskForm):
+    pitch_title = StringField('Pitch Title', validators=[Required()])
+    pitch_category = StringField('Category', validators=[Required()])
+    pitch_prose = TextAreaField('Pitch', validators=[Required()])
+    submit = SubmitField('Submit')
--- app/models.py
+++ app/models.py
@@ -2,6 +2,7 @@ from . import db
 from werkzeug.security import generate_password_hash,check_password_hash
 from flask_login import UserMixin
 from . import login_manager
+from datetime import datetime
 
 
 class User(UserMixin, db.Model):
@@ -36,3 +37,23 @@ class User(UserMixin, db.Model):
         return User.query.get(int(user_id))
 
 
+class Pitch(db.Model):
+
+    __tablename__ = 'pitches'
+
+    id = db.Column(db.Integer,primary_key = True)
+    pitch_id = db.Column(db.Integer)
+    pitch_title = db.Column(db.String)
+    pitch_category= db.Column(db.String)
+    pitch_prose = db.Column(db.String)
+    posted = db.Column(db.DateTime,default=datetime.utcnow)
+    user_id = db.Column(db.Integer,db.ForeignKey("users.id"))
+
+    def save_pitch(self):
+        db.session.add(self)
+        db.session.commit()
+
+    @classmethod
+    def get_pitch(cls, id):
+        pitch = Pitch.query.filter_by(pitch_id=id).all()
+        return pitch
